66aa92471c40ba5f478badc52d4afc2bd5e5a033
GautamAdhish <np01nt4a250076@islingtoncollege.edu.np>
2026-08-14 23:43:29 +0545

    Minor Configuration Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/IS-Audit/',
+  base: '/is-audit/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
