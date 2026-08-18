a99dfedca04a225d6486d05339f39b77a7120e5f
GautamAdhish <np01nt4a250076@islingtoncollege.edu.np>
2026-08-19 00:39:37 +0545

    Port Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/IS-Audit/',
+  base: '/is-audit/',
   build: {
     outDir: '../dist',  // output to project root/dist, not client/dist
   },
